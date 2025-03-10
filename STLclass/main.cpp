9eed501bea63bed4ebded740b6868abf67c5586f
LTH <sigun@LTH>
2025-03-10 17:47:37 +0900

    낡은 코딩 exit

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -30,6 +30,10 @@ void save(std::string_view filename)
 	// 1. 인자로 전달된 파일을 읽기모드로 연다.
 	std::ifstream in{ filename.data()};	// 하위 호환성을 위해 최신의 기술을 맞춘다. 고로 string_view에서 data()를 이용하여 ifstream이 받아들일 수 있는 데이터만 뽑아낸다.
 										// RAII  :  in.close()사용하지 않아도 됌
+	if (not in) {						// !적지 말것
+		std::cout << filename << "can't be opened." << std::endl;
+		exit(20250310);
+	}
 
 	// 2. 쓰기 모드로 저장할 파일을 연다. 덧붙이기 모드로 
 	// 3. 파일을 읽어서 저장할 파일에 덧붙인다. 
