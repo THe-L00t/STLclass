5cd62c46038ba36be4c1f7053dfca3157efa6ed4
LTH <sigun@LTH>
2025-03-10 19:04:22 +0900

    시간 형식 꾸미기

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -31,9 +31,9 @@ int main( char argc, char* argv)
 void save(std::string_view filename)
 {
 	// 1. 인자로 전달된 파일을 읽기모드로 연다.
-	std::ifstream in{ filename.data()};	// 하위 호환성을 위해 최신의 기술을 맞춘다. 고로 string_view에서 data()를 이용하여 ifstream이 받아들일 수 있는 데이터만 뽑아낸다.
-										// RAII  :  in.close()사용하지 않아도 됌
-	if (not in) {						// !적지 말것
+	std::ifstream in{ filename.data()};	
+										
+	if (not in) {						
 		std::cout << filename << "can't be opened." << std::endl;
 		exit(20250310);
 	}
@@ -47,15 +47,21 @@ void save(std::string_view filename)
 
 	auto now = std::chrono::system_clock::now();	// epoch로 부터 경과된 tick 수를 얻는다.
 	// epoch 시간의 기원 1970.1.1
+	auto utc = std::chrono::system_clock::to_time_t(now);	// UTC
+	auto lt = std::localtime(&utc);							// C언어로 만들어진 함수라 레퍼런스 사용 불가, 그래서 &를 붙여 주소를 넘겨주어야 한다. * 지역시간으로 변경
+	auto old = out.imbue(std::locale("ko_KR"));						// 해당 객체를 각 지역에 맞게 바꾸어 버리는 것
+
+
 	using namespace std::chrono_literals;
 	now += 9h;
 
 	out << '\n' << '\n';
 	out << "=================================================" << '\n';
-	out << " 저장한 시간 : " << now << '\n';
+	out << " 저장한 시간 : " << std::put_time(lt,"%c %A") << '\n';
 	out << "=================================================" << '\n';
 	out << '\n';
 
+	out.imbue(old);											// 스트림의 지역을 변경 이전으로 복원
 	// 3. 파일을 읽어서 저장할 파일에 덧붙인다. 
 	std::copy(std::istreambuf_iterator{ in }, {}, std::ostreambuf_iterator{ out });
 }
