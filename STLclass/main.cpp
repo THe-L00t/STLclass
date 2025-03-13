504e93f7a67c1829b7409014247123ca48c3fccc
LTH <sigun@LTH>
2025-03-13 18:07:58 +0900

    문제 + 개인풀이

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -13,7 +13,24 @@
 
 
 // [문제] main을 변경하지 않고 의도대로 실행되게 코딩하라 
+class Dog {
+public:
+	Dog(int a) : n{ a } {
+
+	}
+
+	int& outn();
+	void inn(int&);
+
+	friend std::ostream& operator<<(std::ostream& ot, const Dog& rhs) {
+		ot << rhs.n;
+		return ot;
+	}
+private:
+	int n;
+};
 void change(int&, int&);
+void change(Dog&, Dog&);
 
 int main( char argc, char* argv) 
 {
@@ -39,4 +56,21 @@ void change(int& a, int& b)
 	int temp{ a };
 	a = b;
 	b = temp;
+}
+
+void change(Dog& a, Dog& b)
+{
+	int temp{ a.outn() };
+	a.inn(b.outn());
+	b.inn(temp);
+}
+
+int& Dog::outn()
+{
+	return n;
+}
+
+void Dog::inn(int& a)
+{
+	n = a;
 }
