cc8ce2f6b5c8f45294b3f097b2ea739f5d44ba41
LTH <sigun@LTH>
2025-03-13 17:52:57 +0900

    change문제 및 개인 풀이

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -2,7 +2,7 @@
 // 2025.   1.  STL 월 910 수 910					3월 10일 월요일		(1주 2일)
 //---------------------------------------------------------------------------//
 //
-//	한 학기 강의를 저장할 save기능 
+//	Generic Programming
 // 
 //---------------------------------------------------------------------------
 //
@@ -12,13 +12,23 @@
 #include "save.h"
 
 
-// [문제] save를 제대로 코딩해야죠!
-
+// [문제] main을 변경하지 않고 의도대로 실행되게 코딩하라 
+void change(int&, int&);
 
 int main( char argc, char* argv) 
 {
-	std::cout << "2025년 1학기 STL" << std::endl;
 	
+	int a{ 1 }, b{ 2 };
+
+	change(a, b);
+	std::cout << a << "," << b << std::endl;	// [출력] 2,1
 
 	save("main.cpp");
+}
+
+void change(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
 }
