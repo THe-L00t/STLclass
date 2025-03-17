ab81fc63811f506c6324a598fa62f9b9fb9092a0
LTH <sigun@LTH>
2025-03-17 17:50:32 +0900

    print함수

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -8,15 +8,26 @@
 //
 // 
 #include <iostream>	// 헤더 파일의 전체 코드를 가져온다. 주석은 모두 지운다. 
+#include <random>
+#include <print>
 #include "save.h"
 
 // 컴파일러의 동작
 
-
+// [문제] 랜덤한 int 값 1000개를 화면에 출력하라
+//std::default_random_engine dre;
+std::mt19937_64 dre;								//값을 뽑아내고
+std::uniform_int_distribution uid{0,9999'9999};	//가공한다.
 
 int main( char argc, char* argv) 
 {
-	
+	int n[1000];
+	for (int& a : n) {
+		a = uid(dre);
+	}
+	for (int& a : n) {
+		std::print("{:10}", a);
+	}
 	save("main.cpp");
 }
 
