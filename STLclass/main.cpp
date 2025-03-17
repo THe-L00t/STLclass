bc967901ddcc37cbfb1f38045c92dedae683b53b
LTH <sigun@LTH>
2025-03-17 18:31:32 +0900

    교수님 풀이

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -10,6 +10,7 @@
 #include <iostream>	// 헤더 파일의 전체 코드를 가져온다. 주석은 모두 지운다. 
 #include <random>
 #include <print>
+#include <array>
 #include "save.h"
 
 // 컴파일러의 동작
@@ -18,18 +19,19 @@
 // 저장한 값 중에서 가장 큰 값이 무엇인지 찾아 화면에 출력하라 
 // 출력된 값을 답안지에도 써라 
 
-std::default_random_engine dre;
+std::default_random_engine dre{ };
 //std::mt19937_64 dre;								//값을 뽑아내고
 std::uniform_int_distribution uid{0,9'999'999};	//가공한다.
 
 int main( char argc, char* argv) 
 {
-	int max = -1;
-	int n[1000];
+	std::array<int, 1'000> n;	// T[n] --> array<T,n> ;
+	int max = -1;		//numeric_limits<int>::min(); 이용하기 
+	//	일반적인 for문이라면 hard_wired coding이다. 이때 들어가는 1000이라는 숫자는 magic number.
 	for (int& a : n) {
 		a = uid(dre);
 	}
-	for (int& a : n) {
+	for (const int& a : n) {
 		std::print("{:8}", a);	
 		if (max < a) max = a;
 	}
@@ -37,4 +39,6 @@ int main( char argc, char* argv)
 	save("main.cpp");
 }
 
+// STL이라도 적재적소에 사용하기
+// 최소 자원과 최소 노력
 
