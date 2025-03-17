fc39a56e395961486456d25aa24caef6b324dcd0
LTH <sigun@LTH>
2025-03-17 17:27:22 +0900

    많은 수의 자료를 다루기

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -2,75 +2,22 @@
 // 2025.   1.  STL 월 910 수 910					3월 10일 월요일		(2주 2일)
 //---------------------------------------------------------------------------//
 //
-//	Generic Programming
+//	많은 수의 자료를 다루기 - int 
 // 
 //---------------------------------------------------------------------------
 //
 // 
-//
 #include <iostream>	// 헤더 파일의 전체 코드를 가져온다. 주석은 모두 지운다. 
 #include "save.h"
 
 // 컴파일러의 동작
 
-class Dog {
-public:
-	Dog() = default;
-	Dog(int a) : n{ a } {}
-
-	int& outn();
-	void inn(int&);
-
-	friend std::ostream& operator<<(std::ostream& ot, const Dog& rhs) {
-		ot << rhs.n;
-		return ot;
-	}
-private:
-	int n;
-};
-
-
-template <class T>
-void change(T&, T&);
-
 
 
 int main( char argc, char* argv) 
 {
-	{
-		Dog a{ 1 }, b{ 2 };
-		change(a, b);		
-		std::cout << a << "," << b << std::endl;	// [출력] 2,1
-	}
-
-	{
-		int a{ 1 }, b{ 2 };
-
-		change(a, b);
-		std::cout << a << "," << b << std::endl;	// [출력] 2,1
-	}
-
+	
 	save("main.cpp");
 }
 
 
-int& Dog::outn()
-{
-	return n;
-}
-
-void Dog::inn(int& a)
-{
-	n = a;
-}
-
-template<class T>
-void change(T& a, T& b)
-{
-	T temp{ a };
-	a = b;
-	b = temp;
-}
-
-// 자료구조와 알고리즘은 자료형과 상관없이 적용할 수 있다. generic할 수 있다. 결국 tempalte가 가장 잘 어울린다. 
-//
