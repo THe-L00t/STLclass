52462118c322011bbe57d1641f4365defa0234bc
LTH <sigun@LTH>
2025-03-17 19:00:52 +0900

    새로운 문제 파일에서 값을 읽어와라

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -15,12 +15,10 @@
 
 // 컴파일러의 동작
 
-// [문제] 랜덤 int값 1000개 파일 "int 1000개.txt"에 저장하라. - 텍스트로  int를 저장한다. 
-// 저장한 파일은 사람이 읽을 수 있어야한다.
-// 나중에 파일에서 int 값을 읽어올 수 있어야 한다. 
+// [문제] "int 1000개.txt" 파일에서 가장 큰 값을 찾아 화면에 출력하라.
+// 출력된 값도 답지에 적어라 
 
-std::default_random_engine dre{ };
-//std::mt19937_64 dre;								//값을 뽑아내고
+std::default_random_engine dre{ };				//값을 뽑아내고
 std::uniform_int_distribution uid{0,9'999'999};	//가공한다.
 
 int main( char argc, char* argv) 
@@ -29,10 +27,10 @@ int main( char argc, char* argv)
 	std::ofstream out{"int 1000개.txt"};
 	int cnt{ 0 };
 	for (size_t i = 0; i < 1000;++i) {
-		//out << uid(dre) << ' ';		// 구분자 delemeter는 뭐라도 상관없다. 
 		std::print(out, "{:8}", uid(dre));
 		if (not(++cnt % 10)) out << std::endl;
 	}
+
 	{
 		std::ifstream in{ "int 1000개.txt" };
 		if (not in) return 404;
