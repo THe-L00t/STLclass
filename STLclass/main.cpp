9318b4cc109b3995926e0e133fa2a6d0966d16b5
LTH <sigun@LTH>
2025-03-24 17:54:43 +0900

    클래스와 static변수

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -2,33 +2,41 @@
 // 2025.   1.  STL 월 910 수 910					3월 10일 월요일		(3주 2일)
 //---------------------------------------------------------------------------//
 //
-//	많은 수의 자료를 다루기 - int 를 메모리 크기 그대로 저장해야~
+//	많은 수의 자료를 다루기 - Dog를 읽고 쓸 수 있어야
 // 
 //---------------------------------------------------------------------------
 //
 // 
 #include <iostream>	
+#include <string>
 #include <random>
-#include <fstream>
-#include <array>
-#include <print>
 #include "save.h"
 
+// Dog 객체 10만개를 파일 "Dog 10만마리"에 저장한다. 
 
-std::default_random_engine dre{ };				
-std::uniform_int_distribution uid{0,9'999'999};	
+std::default_random_engine dre;
+std::uniform_int_distribution uid{'a','z'};
+
+class Dog {
+public:
+	Dog() : id{ ++sid } {
+		for (size_t i = 0; i < 15; i++)
+		{
+			name += uid(dre);
+		}
+	};
+private:
+	std::string name;	// 15글자의 random 소문자로 구성
+	size_t id;			// 생성될때마다 고유번호 부여
+
+	static int sid;		// scope:local, life-time:global
+};
+
+int Dog::sid{};
 
 int main( char argc, char* argv) 
 {
 	
-	std::ifstream in{ "진짜 랜덤 10만개",std::ios::binary };
-
-	std::array<int, 10'0000> arr;
-	in.read((char*)arr.data(), sizeof(int) * arr.size());
-	
-	// structured binding
-	auto [최솟값, 최댓값] = std::minmax_element(arr.begin(), arr.end());
-	std::cout << "최솟값 - " << *최솟값 << "최댓값 - " << *최댓값 << std::endl;
 
 	save("main.cpp");
 }
