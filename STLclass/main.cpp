9f19b14a20ee84d114ab32b65acc830aa8e5f6a0
LTH <sigun@LTH>
2025-03-27 18:16:28 +0900

    free store 감상하기

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -10,41 +10,26 @@
 #include <iostream>	
 #include <numeric>
 #include "save.h"
-
-// [문제] 양의 정수(int의 최댓값)를 입력받아 num에 저장하라 
-// int값을 num개 저장할 수 있는 메모리를 확보하라.
-// 메모리의 내용을 1부터 시작하는 정수로 채워라. 
-// 메모리에 있는 값의 합계를 화면에 출력하라
-// 이 과정을 영원히 반복하라 
-
+ 
+void f() {
+	int* p = new int[5];
+	std::cout << "번지 - " << p << std::endl;
+	for (size_t i = 0; i < 5; i++)
+	{
+		std::cout << p[i] << std::endl;
+	}
+	delete[] p;
+}
 
 int main( ) 
 {
-
-	save("main.cpp");
-	while (true) {
-		std::cout << "양의 정수를 입력하라 : ";
-		int num;
-		std::cin >> num;
-		if (num < 1) {
-			std::cout << " 다시 입력하세요 " << std::endl;
-		}
-		else {
-			//std::unique_ptr<int> numbers{ new int[num]() };
-			int* numbers = new int[num];
-			//__int64 sum;
-			std::cout << std::numeric_limits<long long>::max() << std::endl;
-			
-			std::iota(numbers, numbers + num, 1);
-			long long sum = std::accumulate(numbers, numbers + num, 0LL);
-			/*for (size_t i = 0; i < num; i++)
-			{
-				sum += numbers[i];
-			}*/
-			std::cout << "1부터 " << num << "까지의 합계 - " << sum << std::endl;
-			delete[] numbers;
-		}
+	for (size_t i = 0; i < 10; i++)
+	{
+		f();
 	}
+	
+	save("main.cpp");
+
 }
 
 
