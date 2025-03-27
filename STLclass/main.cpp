2779a18c22d84f88f3c2b656d764b5ff0fc8478c
LTH <sigun@LTH>
2025-03-27 17:49:28 +0900

    malloc 과 new 의 차이

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -8,15 +8,46 @@
 //
 // 
 #include <iostream>	
-
 #include "save.h"
 
-
+// [문제] 양의 정수(int의 최댓값)를 입력받아 num에 저장하라 
+// int값을 num개 저장할 수 있는 메모리를 확보하라.
+// 메모리의 내용을 1부터 시작하는 정수로 채워라. 
+// 메모리에 있는 값의 합계를 화면에 출력하라
+// 이 과정을 영원히 반복하라 
 
 
 int main( ) 
 {
 
 	save("main.cpp");
+	while (true) {
+		std::cout << "양의 정수를 입력하라 : ";
+		int num;
+		std::cin >> num;
+		if (num < 1) {
+			std::cout << " 다시 입력하세요 " << std::endl;
+		}
+		else {
+			//std::unique_ptr<int> numbers{ new int[num]() };
+			int* numbers = new int[num];
+			//__int64 sum;
+			long sum;
+			for (size_t i = 0; i < num; i++)
+			{
+				numbers[i] = i+1;
+			}
+			for (size_t i = 0; i < num; i++)
+			{
+				sum += numbers[i];
+			}
+			std::cout << "1부터 " << num << "까지의 합계 - " << sum << std::endl;
+			sum = 0;
+		}
+	}
 }
 
+
+// 중요한 내용
+// C의 malloc는 void*를 반환한다.  //heap
+// c++의 new 는 가공한 주소를 반환한다.  //free store
