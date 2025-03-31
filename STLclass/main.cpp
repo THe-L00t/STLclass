777c4eb31a8926c1025b74aba0ca1b10d667fc52
LTH <sigun@LTH>
2025-03-31 18:48:42 +0900

    유연한 프로그래밍 & 함수 포인터

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -16,8 +16,8 @@
 #include <ranges>
 #include "save.h"
  
-
-std::default_random_engine dre;
+std::random_device rd;
+std::default_random_engine dre{ rd() };
 std::uniform_int_distribution uid(0, 999'9999);
 // [문제] 값의 범위가 [0,1'000'0000) 까지인 random int값 천 만개를 저장할 
 // 메모리를 확보하고 값을 채워라
@@ -27,14 +27,20 @@ std::uniform_int_distribution uid(0, 999'9999);
 std::array<int, 1000'0000> numbers;
 
 int 오름차순(const void* a, const void* b);
+int 내림차순(const void* a, const void* b);
 
 int main( ) 
 {
+	int (*정렬방법)(const void*, const void*);
 	//std::unique_ptr<std::array<int, 1000'0000>> numbers{ new std::array<int,1000'0000> };
 	for (int& num : numbers) {
 		num = uid(dre);
 	}
-	qsort(numbers.data(), numbers.size(), sizeof(std::array<int,1000'0000>::value_type), 오름차순);
+	
+	if (numbers[0] % 2 == 0 )정렬방법 = 오름차순;
+	else 정렬방법 = 내림차순;
+
+	qsort(numbers.data(), numbers.size(), sizeof(int), 정렬방법);
 	for (int& num : numbers | std::views::take(1000))
 	{
 		std::print("{:8}", num);
@@ -46,6 +52,15 @@ int main( )
 }
 
 int 오름차순(const void* a, const void* b)
+{
+	int A = *(int*)a;
+	int B = *(int*)b;
+	if (A < B) return -1;
+	else if (B < A) return 1;
+	return 0;
+}
+
+int 내림차순(const void* a, const void* b)
 {
 	int A = *(int*)a;
 	int B = *(int*)b;
