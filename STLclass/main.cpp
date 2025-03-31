0822ddbc985b262c9b7b40e5d554aac8500bd350
LTH <sigun@LTH>
2025-03-31 19:00:54 +0900

    sort의 특징

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -26,51 +26,23 @@ std::uniform_int_distribution uid(0, 999'9999);
  
 std::array<int, 1000'0000> numbers;
 
-int 오름차순(const void* a, const void* b);
-int 내림차순(const void* a, const void* b);
 
 int main( ) 
 {
 
-	// 두 가지 예외사항
-	// int[10] a;   X
-	// int(*)(const void*,const void*) fp;   X
-
-
-	int (*정렬방법)(const void*, const void*);
-	//std::unique_ptr<std::array<int, 1000'0000>> numbers{ new std::array<int,1000'0000> };
 	for (int& num : numbers) {
 		num = uid(dre);
 	}
-	
-	if(numbers[0] %2 == 1)정렬방법 = 오름차순;
-	else 정렬방법 = 내림차순;
 
-	qsort(numbers.data(), numbers.size(), sizeof(int), 정렬방법);
+	//STL의 sort 정렬
+	sort(numbers.begin(), numbers.end());	// generic, contiguous
+
 	for (int& num : numbers | std::views::take(1000))
 	{
 		std::print("{:8}", num);
-		//std::cout << numbers[i];
 	}
 
 	save("main.cpp");
 
 }
 
-int 오름차순(const void* a, const void* b)
-{
-	int A = *(int*)a;
-	int B = *(int*)b;
-	if (A < B) return -1;
-	else if (B < A) return 1;
-	return 0;
-}
-
-int 내림차순(const void* a, const void* b)
-{
-	int A = *(int*)a;
-	int B = *(int*)b;
-	if (A < B) return 1;
-	else if (B < A) return -1;
-	return 0;
-}
