08db1122ef14da09541e2637ae092a60295172ae
LTH <sigun@LTH>
2025-04-03 17:41:26 +0900

    최신 sort설명

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -24,7 +24,7 @@ std::default_random_engine dre{ rd() };
 std::uniform_int_distribution uid(0, 999'9999);
 // [문제] 값의 범위가 [0,1'000'0000) 까지인 random int값 천 만개를 저장할 
 // 메모리를 확보하고 값을 채워라
-// (시작) qsort로 오름차순 정렬하라.
+// std::sort를 사용하여 오름차순으로 정렬하라
 // 정렬한 결과 중에서 앞에서 부터 1000개만 화면 출력하라
  
 std::array<int, 1000'0000> numbers;
@@ -43,7 +43,7 @@ int main( )
 	//STL의 sort 정렬
 	//정렬에 걸리는 시간
 	auto b = std::chrono::high_resolution_clock::now();//스톱워치 시작
-	sort(numbers.begin(), numbers.end(),내림차순 );	// generic, contiguous
+	std::ranges::sort(numbers.begin(), numbers.end());	// 디폴트 오름차순
 	auto e = std::chrono::high_resolution_clock::now();//스톱워치 끝
 
 	for (int& num : numbers | std::views::take(1000))
@@ -51,7 +51,13 @@ int main( )
 		std::print("{:8}", num);
 	}
 
-	std::cout << std::endl << "경과시간 - " << e-b << std::endl;
+	std::cout << std::endl << "경과시간(duration) - " << e-b << std::endl;
+	std::cout << "경과시간(1/1000초) - " << std::chrono::duration_cast<std::chrono::milliseconds>(e - b) << std::endl;
+
+	// sort로 내림차순 
+
+
+
 	save("main.cpp");
 
 }
