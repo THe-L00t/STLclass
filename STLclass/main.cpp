3e0175c7920ce5b74a983fc793d9427dda4e4229
LTH <sigun@LTH>
2025-04-17 18:58:04 +0900

    벡터 관찰하기

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -10,17 +10,23 @@
 //
 // 
 #include <iostream>	
-#include <fstream>
-#include <array>
-#include <algorithm>
+#include <vector>
+#include <memory>
 #include "save.h"
 #include "STRING.h"
 
+std::vector<int> v1;
 
 extern bool °üÂû;
 int main( ) 
 {
-	
-	save("main.cpp");
+	std::vector<int> v{1};
+	std::unique_ptr<std::vector<int>> v2 = std::make_unique<std::vector<int>>(1);
+
+	std::cout << sizeof(v) << " " << addressof(v) << " " << typeid (v).name() << std::endl;
+	//std::cout << sizeof(v) << " " << v.data( ) << " " << typeid (v).name() <<  std::endl;
+	std::cout << sizeof(v1) << " " << addressof(v1) << " " << typeid (v1).name() << std::endl;
+	std::cout << sizeof(*v2.get()) << " " << v2.get() << " " << typeid (v2.get()).name() << std::endl;
+ 	//save("main.cpp");
 }
 
