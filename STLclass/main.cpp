71c5927e6b5243b2ece07780648b065e50cc386a
LTH <sigun@LTH>
2025-04-28 18:34:27 +0900

    C++에서는 속도를 중요시 한다.

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -24,8 +24,11 @@ extern bool 관찰;
 int main( ) 
 {
     int a[10];
-    std::cout << a[10] << std::endl;
-    // 기존 배열의 문제
+    
+    for (int i = -10; i < 10; i++) //이 경우 읽기와 쓰기가 다르게 동작한다.
+    {//쓰기의 경우 죽는다
+        std::cout << a[i] << std::endl; //읽기의 경우 c++이 메모리 접근을 모두 허락하기에 정상적으로 동작한다.  
+    }
 
  	save("main.cpp");
 }
