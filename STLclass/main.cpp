d3eaa2bb49c8bf4e395e51a90e80d0bfcdbb292e
LTH <sigun@LTH>
2025-05-15 17:47:34 +0900

    list의 merge 함수

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -13,10 +13,7 @@
 //
 // 
 #include <iostream>	
-#include <fstream>
 #include <list>
-#include <vector>
-#include <algorithm>
 #include "save.h"
 #include "STRING.h"
 
@@ -24,24 +21,13 @@ extern bool 관찰;
 
 int main( ) 
 {
-    // [문제] "main.cpp"의 단어를 list에 모두 저장하라.
-    std::ifstream in{ "main.cpp" };
-    if (not in) return 2024182028;
+    std::list<STRING> 할일1{ "아가리오", "게임", "설겆이", "컴퓨터 구조"};
+    std::list<STRING> 할일2{ "과제제출", "조명 인수인계", "취침", "등등등"};
 
-    std::list<STRING> m{ std::istream_iterator<STRING>{in},{} }; //한 줄로 줄임
-    
-    // [문제] 찾을 단어가 있으면 몇 번째인지 출력 
-    std::cout << "찾을 단어 : ";
-    STRING 단어;
-    std::cin >> 단어;
+    할일1.merge(할일2);
 
-    auto 위치 = std::find(m.begin(), m.end(), 단어);
-    if (위치 == m.end()) { 
-        std::cout << "없는 단어 입니다. " << std::endl; 
-    }
-    else {
-        std::cout << 단어 << "는 " << std::distance(m.begin(), 위치) + 1 << "번째 단어 입니다." << std::endl;
-        // 지난 시간 작성한 for루프 코드를 대신 수행 해줌
+    for (const STRING& s : 할일2) {
+        std::cout << s << std::endl;
     }
 
  	save("main.cpp");
