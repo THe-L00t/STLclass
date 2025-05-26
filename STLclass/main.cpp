f2e0c91e397952bfdf61b73300b08c922827c2fb
LTH <sigun@LTH>
2025-05-26 18:48:18 +0900

    새로운 문제

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -31,25 +31,8 @@ const It& my_find(const It& s, const It& e, const T& val) {
 int main( ) 
 {
     STRING s{ "2025. 5. 26" };
-    //[문제] 한 글자를 입력받아 s에 있는지 없는지, 있다면 몇 번째 글자인지 출력하라
-    char c{};
-    std::cin >> c;
-    auto p = my_find(s.begin(), s.end(), c);
-    // 해당 함수를 완성하라 
-
-    if(p != s.end())  std::cout << std::distance(s.begin(), p) + 1 << "번째 입니다." << std::endl;
-    else  std::cout << "없는 문자 입니다." << std::endl;
-    //STRING_Iterator cusor = std::find(s.begin(), s.end(), c);
-    //if (cusor != s.end()) {
-    //    //// random access iterator tag 라면 
-    //    //std::cout << cusor - s.begin() + 1 << "번째 입니다." << std::endl;
-    //    ////아니라면 개수를 세 나아가야 한다. 
-    //    //위와 같은 일을 하는 것이 distance함수 이다. 
-    //    std::cout << std::distance(s.begin(), cusor) + 1 << "번째 입니다." << std::endl;
-    //}
-    //else {
-    //    std::cout << "없는 문자 입니다." << std::endl;
-    //}
+    //[문제] 3보다 큰 숫자를 찾아라.
+    
 
  	save("main.cpp");
 }
