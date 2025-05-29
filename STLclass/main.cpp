21986ad314a6d9deb2084d2124b174b01fdc4344
LTH <sigun@LTH>
2025-05-29 18:51:34 +0900

    [문제] vector에 txt파일 불러오기

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -3,24 +3,31 @@
 // 2025. 6. 19 기말 시험
 // 2025. 5. 12 월요일 - 졸업작품 중간발표
 //---------------------------------------------------------------------------//
-//  반복자와 알고리즘
-//  find() 
-//  find_if()
-//  copy()
+//  Associative Container
+//  - set<key>          : key 와 value를 구분하지 않는다.
+//  - map<key, value>   : 모델링 - texture, <아이돌그룹, 멤버>
+//  
 //---------------------------------------------------------------------------
 //
 // 
 #include <iostream>	
-#include <algorithm>
+#include <fstream>
 #include <vector>
+#include <set>
 #include "save.h"
 #include "STRING.h"
 
 extern bool 관찰;
-
+// [문제] 이상한 나라의 앨리스.txt를 다운받는다.
+// 1. 파일을 vector<STRING> v;에 저장하라.
+// 2. 모두 몇 단어로 만들었는지 출력하라.
 
 int main( ) 
 {
+    std::ifstream in{ "이상한 나라의 앨리스.txt" };
+    if (not in) { return 404; }
+
+    std::vector<STRING> v{ std::istream_iterator<STRING>{in}, std::istream_iterator<STRING>{} };
 
 
  	save("main.cpp");
