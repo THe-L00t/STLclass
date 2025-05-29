0d4761dd85d9048f9b3d6341fdd6d46bcaf833c5
LTH <sigun@LTH>
2025-05-29 18:16:55 +0900

    set과 map 시작

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -17,50 +17,11 @@
 #include "STRING.h"
 
 extern bool 관찰;
-template<class It, class T>
-const It& my_find(const It& s, const It& e, const T& val) {
-    It now = s;
-    while (now != e) {
-        if (*now == val) return now;
-        ++now;
-    }
-    return now;
-}
 
-template<class It, class T>
-const It& my_find_if(const It& s, const It& e, T callable) {
-    It now = s;
-    while (now != e) {
-        if (callable(*now)) return now; // 프레더킷 : 판단해주는 놈
-        ++now;
-    }
-    return now;
-}
-
-template<class Fit, class Sit>
-void my_copy(Fit s, Fit e, Sit d) {
-    while (s not_eq e) {
-        *d = *s;            // v.push_back(*s);
-        ++s;                // 아무것도 안한다. 
-        ++d;
-    }
-}
-// SFINAE : constexpr 에 대한 설명
 
 int main( ) 
 {
-    STRING s{ "2025. 5. 29" };
 
-    //[문제] s의 문자를 화면에 복사 
-    std::vector<char> v;
-
-    my_copy(s.begin(), s.end(), std::back_inserter(v));
-    
-    for (char c : v) {
-        std::cout << c << std::endl;
-    }
-
-    std::cout << std::endl;
 
  	save("main.cpp");
 }
