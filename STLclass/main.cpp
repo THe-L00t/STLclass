f9928d20222d163cb853255a166ec705dbb27b81
LTH <sigun@LTH>
2025-06-02 17:27:11 +0900

    0602 시작

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------//
-// 2025.   1.  STL 월 910 목 910					5월 19일 월요일		(12주 2일)
+// 2025.   1.  STL 월 910 목 910					6월 2일 월요일		(13주 1일)
 // 2025. 6. 19 기말 시험
 // 2025. 5. 12 월요일 - 졸업작품 중간발표
 //---------------------------------------------------------------------------//
@@ -19,7 +19,7 @@
 
 extern bool 관찰;
 // [문제] 이상한 나라의 앨리스.txt를 다운받는다.
-// 1. 파일을 vector<STRING> v;에 저장하라.
+// 1. 파일을 set<STRING> s;에 저장하라.
 // 2. 모두 몇 단어로 만들었는지 출력하라.
 
 int main( ) 
@@ -27,10 +27,10 @@ int main( )
     std::ifstream in{ "이상한 나라의 앨리스.txt" };
     if (not in) { return 404; }
 
-    std::set<STRING> v{ std::istream_iterator<STRING>{in}, std::istream_iterator<STRING>{} };
-    std::cout << "총 " << v.size() << "단어 입니다. " << std::endl;
+    std::set<STRING> s{ std::istream_iterator<STRING>{in}, std::istream_iterator<STRING>{} };
+    std::cout << "총 " << s.size() << "단어 입니다. " << std::endl;
 
-    // while 사용할 때와 현제 비교해보기 
+    // while 사용할 때와 현재 비교해보기 
     //reserve 값 바꿔 비교해보기 
 
  	save("main.cpp");
