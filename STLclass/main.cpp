50fc5728cec28eee92908ad231d6d1fd4279f95d
THe-L00t <sigun2001@naver.com>
2025-06-12 18:56:00 +0900

    distance 시작

--- STLclass/main.cpp
+++ STLclass/main.cpp
@@ -3,60 +3,26 @@
 // 2025. 6. 19 기말 시험
 //---------------------------------------------------------------------------//
 //  
-//  STL sort 관련 알고리즘 - 복잡도 순서대로 
-//  1. partition
-//  2. nth_element
-//  3. partial_sort
-//  4. sort
-//  5. stable_sort
-//  6. 
-// 
+//  distance 구현 - tag dispatch, if constexpr, concept, SFINAE
 //---------------------------------------------------------------------------
-// 과제 관련 : adjacent_find
+// 
 // 
 #include <iostream>
 #include <vector>
+#include <list>
 #include <algorithm>
-#include <print>
-#include <random>
-#include <numeric>
-#include <ranges>
 #include "save.h"
 #include "STRING.h"
 
 using namespace std;
-std::default_random_engine dre{ std::random_device{}()};
-uniform_int_distribution<int> uidChar{ 'A','Z' };
-uniform_int_distribution uid{ 1,50 };
+
 extern bool 관찰;
 
-struct Dog {
-    char c{static_cast<char>(uidChar(dre))};
-    int n{uid(dre)};
-
-    friend ostream& operator<<(std::ostream& os, const Dog& d) {
-        print(os, "[{:}] - {:3}", d.c, d.n);
-        return os;
-    }
-};
 
 int main( ) 
 {
-    vector<Dog> dogs(100);
-    /*sort(dogs.begin(), dogs.end(), [](const Dog& a, const Dog& b) {
-        return a.c > b.c;
-        });*/
-
-    ranges::sort(dogs, {},&Dog::c);
-    for (const Dog& d : dogs) {
-        cout << d << endl;
-    }
-
-    cout << "상대적인 순서를 유지하며 dogs를 n기준으로 다시 돌아간다" << endl;
-    ranges::stable_sort(dogs, {}, &Dog::n);
-    for (const Dog& d : dogs) {
-        cout << d << endl;
-    }
+    vector<int> v{ 1,2,3,4,5 };
+    cout << distance(v.begin(), v.end()) << endl;
  	save("main.cpp");
 }
 
